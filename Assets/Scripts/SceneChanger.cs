968faa6196cf48f45b4c9d831ee4cd4abf63718a
Daniel Gaviria <dgaviria@endava.com>
2019-07-26 09:05:59 -0500

    Added Inicial Configuration

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void changeScene ( string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
