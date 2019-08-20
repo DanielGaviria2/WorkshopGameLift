11770ca1ce09950d057c5441a1a4421e6c4bcc4d
Miguel Mejia <miguel.mejia@endava.com>
2019-08-20 10:31:40 -0500

    Added maze generation and wall creation and destruction.

---
+++ Assets/Scripts/HideDuplicate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideDuplicate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        this.gameObject.SetActive(false);
+        Debug.Log("cosas");
+    }
+    
+}
---
+++ Assets/Scripts/HideOnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideOnClick : MonoBehaviour
+{ 
+    private void OnMouseDown()
+    {
+        if(this.gameObject.GetComponent<Renderer>().enabled) this.gameObject.GetComponent<Renderer>().enabled = false;
+        else this.gameObject.GetComponent<Renderer>().enabled = true;
+
+    }
+}
